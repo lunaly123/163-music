2c9b156197c9337aa4845ca9cb0d1d4a9b4a0cdb
lunaly123 <dbpangzi@gmail.com>
2018-11-05 00:44:41 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <MHeader/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/header/header'
+export default {
+  name: 'App',
+  components: {
+    MHeader
+  }
+}
+</script>
+
+<style>
+</style>
